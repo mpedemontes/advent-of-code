88e576cf8e5d13c501e4220e10bc43cdd11521f2
mpedemontesalvador <marc.pedemonte@cabsa.es>
2025-12-05 09:58:29 +0100

    day 4 part two

---
+++ src/main/java/year2025/day4/Part2.java
@@ -0,0 +1,84 @@
+package year2025.day4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    List<List<Character>> lines = new ArrayList<>();
+    String line;
+    while (sc.hasNext()) {
+      line = sc.nextLine();
+      if ("fin".equals(line)) {
+        break;
+      }
+      if (!line.isBlank()) {
+        lines.add(new ArrayList<>(line.chars().mapToObj(c -> (char) c).toList()));
+      }
+    }
+    List<Character> currentLine;
+    int surroundingRolls;
+    int total = 0;
+    boolean removed = true;
+    while (removed) {
+      removed = false;
+      List<int[]> toRemove = new ArrayList<>();
+
+      for (int i = 0; i < lines.size(); i++) {
+        currentLine = lines.get(i);
+        for (int j = 0; j < currentLine.size(); j++) {
+          surroundingRolls = 0;
+          if (currentLine.get(j) == '@') {
+            if (i > 0) {
+              surroundingRolls += checkSurroundingLine(lines.get(i - 1), j);
+            }
+            surroundingRolls += checkCurrentLine(lines.get(i), j);
+            if (i + 1 < lines.size()) {
+              surroundingRolls += checkSurroundingLine(lines.get(i + 1), j);
+            }
+            if (surroundingRolls < 4) {
+              toRemove.add(new int[] {i, j});
+            }
+          }
+        }
+      }
+      if (!toRemove.isEmpty()) {
+        removed = true;
+        total += toRemove.size();
+        for (int[] pos : toRemove) {
+          lines.get(pos[0]).set(pos[1], '.');
+        }
+      }
+    }
+    System.out.println(total);
+  }
+
+  private static int checkCurrentLine(List<Character> line, int j) {
+    int total = 0;
+
+    if (j > 0 && line.get(j - 1) == '@') {
+      total++;
+    }
+    if (j + 1 < line.size() && line.get(j + 1) == '@') {
+      total++;
+    }
+    return total;
+  }
+
+  private static int checkSurroundingLine(List<Character> line, int j) {
+    int total = 0;
+
+    if (j > 0 && line.get(j - 1) == '@') {
+      total++;
+    }
+    if (line.get(j) == '@') {
+      total++;
+    }
+    if (j + 1 < line.size() && line.get(j + 1) == '@') {
+      total++;
+    }
+    return total;
+  }
+}
